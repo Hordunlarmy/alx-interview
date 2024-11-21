68e92da5857c8cd621c5d52e4f7561027d26d88a
Hordunlarmy <Hordunlarmy@gmail.com>
2024-11-21 08:06:16 +0100

    refactor: Refactor Star Wars API script for improved readability and efficiency

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  const filmId = process.argv[2];
-  const filmURL = `${API_URL}/films/${filmId}/`;
-
-  request(filmURL, (err, _, body) => {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
-      console.error(err);
-      return;
+      console.log(err);
     }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-    const charactersURLs = JSON.parse(body).characters;
-    const characterPromises = charactersURLs.map(
-      (url) =>
-        new Promise((resolve, reject) => {
-          request(url, (charErr, __, charBody) => {
-            if (charErr) {
-              reject(charErr);
-            } else {
-              resolve(JSON.parse(charBody).name);
-            }
-          });
-        }),
-    );
-
-    Promise.all(characterPromises)
-      .then((names) => console.log(names.join('\n')))
-      .catch((error) => console.error(error));
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
